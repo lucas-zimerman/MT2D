b5083068311206d035e8b43ed2da5da930aeedd2
ibm5155 <lucas-zimerman1@hotmail.com>
2017-08-27 12:34:36 -0300

    -Audio Struct is now better with a better control to choise what sound should stop playing in case all the audio buffers are being used.

--- SDL/Audio/MT2D_SDL_Audio.cpp
+++ SDL/Audio/MT2D_SDL_Audio.cpp
@@ -17,20 +17,41 @@
 #endif
 #include <queue>
 #include <cmath>
+#include <time.h>
 
 extern bool nosound;
 
+/**
+	Private struct
+**/
+typedef struct MT2D_Audio_Buffer {
+#ifdef SDL_USE_AUDIO
+	bool SDL_AUDIO_STARTED;
+	Mix_Music *SDL_music;
+	int music_loaded;
+	Mix_Chunk *SDL_Audio[8]; // we can play only max 8 audio files at the same time.
+	time_t SDL_Audio_Started[8];
+	int sound_loaded;
+#endif
+};
+
+MT2D_Audio_Buffer SDL_ABuffer;
 bool SDL_AUDIO_STARTED = false;
-Mix_Music *SDL_mus = 0;
-int music_loaded = 0;
-Mix_Chunk *SDL_wav = 0;
-int sound_loaded = 0;
 
 extern char SDL_SOUND_TYPE;
 
 void SDL_Start_Sound_System()
 {
-    if(SDL_SOUND_TYPE == -1){
+	SDL_ABuffer.music_loaded = 0;
+	for (int i = 0; i < 8; i++) {
+		SDL_ABuffer.SDL_Audio[i] = 0;
+		SDL_ABuffer.SDL_Audio_Started[i] = 0;
+	}
+	SDL_ABuffer.SDL_AUDIO_STARTED = 0;
+	SDL_ABuffer.SDL_music = 0;
+	SDL_ABuffer.sound_loaded = 0;
+
+	if(SDL_SOUND_TYPE == -1){
 		SDL_SOUND_TYPE = 1;
         if(SDL_Init(SDL_INIT_AUDIO) < 0){
             exit(1);
@@ -41,20 +62,31 @@ void SDL_Start_Sound_System()
     }
 }
 
-#ifdef _WIN32
-Mix_Music *mus = 0;
-
-Mix_Chunk *wav = 0;
-#endif
-
 void SDL_Close_Sound_System()
 {
 	if (SDL_SOUND_TYPE == 1) {
 		while (Mix_Playing(0) != 0);
-		Mix_FreeChunk(SDL_wav);
-		Mix_FreeMusic(SDL_mus);
+		for (int i = 0; i < 8; i++) {
+			if (SDL_ABuffer.SDL_Audio[0]) {
+				Mix_FreeChunk(SDL_ABuffer.SDL_Audio[0]);
+			}
+		}
+		Mix_FreeMusic(SDL_ABuffer.SDL_music);
 		Mix_CloseAudio();
 		SDL_Quit();
+	}
+}
+
+void SDL_Clear_Audio_Buffer() {
+	Mix_Chunk *MC;
+	time_t NOW = clock();
+	for (int i = 0; i < 8; i++) {
+		if (SDL_ABuffer.SDL_Audio[i] != NULL && NOW-SDL_ABuffer.SDL_Audio_Started[i] > SDL_ABuffer.SDL_Audio[i]->alen) {
+			//the sound has already played
+			Mix_FreeChunk(SDL_ABuffer.SDL_Audio[i]);
+			SDL_ABuffer.SDL_Audio[i] = 0;
+			SDL_ABuffer.SDL_Audio_Started[i] = 0;
+		}
 	}
 }
 
@@ -65,37 +97,37 @@ void SDL_Play_Music(char *Name){
 #endif
 		if (Name) {
 #ifdef _DEBUG
-			printf("music loaded: %d \n", music_loaded);
+			printf("music loaded: %d \n", SDL_ABuffer.music_loaded);
 #endif
-			if (!music_loaded) {
+			if (!SDL_ABuffer.music_loaded) {
 #ifdef _DEBUG
 				printf("Entrou no if\n");
 #endif
 
-				SDL_mus = Mix_LoadMUS(Name);
-				if (!SDL_mus) {
+				SDL_ABuffer.SDL_music = Mix_LoadMUS(Name);
+				if (!SDL_ABuffer.SDL_music) {
 #ifdef _DEBUG
 					printf("Mix_LoadMUS(\"%s\")> %s\n", Name, Mix_GetError());
 #endif
 				}
-				music_loaded = 1;
+				SDL_ABuffer.music_loaded = 1;
 #ifdef _DEBUG
 				printf("som na caixa\n");
 #endif
-				Mix_PlayMusic(SDL_mus, 10);
+				Mix_PlayMusic(SDL_ABuffer.SDL_music, 10);
 			}
 			else {
-				if (SDL_mus) {
-					Mix_FreeMusic(SDL_mus);
+				if (SDL_ABuffer.SDL_music) {
+					Mix_FreeMusic(SDL_ABuffer.SDL_music);
 				}
-				SDL_mus = Mix_LoadMUS(Name);
-				if (!SDL_mus) {
+				SDL_ABuffer.SDL_music = Mix_LoadMUS(Name);
+				if (!SDL_ABuffer.SDL_music) {
 #ifdef _DEBUG
 					printf("Mix_LoadMUS(\"%s\")> %s\n", Name, Mix_GetError());
 #endif
 				}
-				music_loaded = 1;
-				Mix_PlayMusic(SDL_mus, 10);
+				SDL_ABuffer.music_loaded = 1;
+				Mix_PlayMusic(SDL_ABuffer.SDL_music, 10);
 			}
 		}
 		else {
@@ -108,18 +140,51 @@ void SDL_Play_Music(char *Name){
 
 void SDL_Play_Sound(char *Name){
 	if (SDL_SOUND_TYPE == 1) {
-		if (!sound_loaded) {
-			sound_loaded = 1;
-			SDL_wav = Mix_LoadWAV(Name);
-			Mix_PlayChannel(0, SDL_wav, 0);
-		}
-		else {
-			sound_loaded = 1;
-			if (SDL_wav) {
-				Mix_FreeChunk(SDL_wav);
+		bool Played = 0;
+ 		int i = 0;
+ 		SDL_Clear_Audio_Buffer();
+		for (; i < 8; i++) {
+			if (SDL_ABuffer.SDL_Audio[i] == 0) {
+				SDL_ABuffer.SDL_Audio[i] = Mix_LoadWAV(Name);
+				Mix_PlayChannel(i, SDL_ABuffer.SDL_Audio[i], 0);
+				SDL_ABuffer.SDL_Audio_Started[i] = clock();
+				Played = true;
+				i = 8;
 			}
-			SDL_wav = Mix_LoadWAV(Name);
-			Mix_PlayChannel(0, SDL_wav, 0);
+
+		}
+		if (Played == false) {
+			/*All the channels are in use so*/
+			/*first we check this audio is already being played*/
+			Mix_Chunk *Tmp = Mix_LoadWAV(Name);
+			for (i = 0; i < 8; i++) {
+				if (SDL_ABuffer.SDL_Audio[i]->volume == Tmp->volume &&
+					SDL_ABuffer.SDL_Audio[i]->alen == Tmp->alen &&
+					SDL_ABuffer.SDL_Audio[i]->allocated == Tmp->allocated) {
+					Mix_FreeChunk(Tmp);
+					Mix_PlayChannel(i, SDL_ABuffer.SDL_Audio[i], 0);
+					SDL_ABuffer.SDL_Audio_Started[i] = clock();
+					i = 8;
+					Played = true;
+				}
+			}
+			if (Played == false) {
+				/*this file isnt being played, so lets replace the sound that were being played that is near to finish*/
+         				int IdChannel = 0;
+				time_t NOW = clock();
+				int ShortestTime = SDL_ABuffer.SDL_Audio[0]->alen - (NOW - SDL_ABuffer.SDL_Audio_Started[0]);
+				for (i = 1; i < 8; i++) {
+					if (SDL_ABuffer.SDL_Audio[i]->alen - (NOW - SDL_ABuffer.SDL_Audio_Started[i]) < ShortestTime)
+					{
+						IdChannel = i;
+						ShortestTime = SDL_ABuffer.SDL_Audio[i]->alen - (NOW - SDL_ABuffer.SDL_Audio_Started[i]);
+					}
+				}
+				Mix_FreeChunk(SDL_ABuffer.SDL_Audio[IdChannel]);
+				SDL_ABuffer.SDL_Audio[IdChannel] = Tmp;
+				Mix_PlayChannel(IdChannel, SDL_ABuffer.SDL_Audio[IdChannel], 0);
+				SDL_ABuffer.SDL_Audio_Started[IdChannel] = clock();
+			}
 		}
 	}
 }
