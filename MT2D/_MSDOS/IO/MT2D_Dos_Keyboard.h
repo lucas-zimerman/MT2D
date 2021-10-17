8d764f0495b956812fc763c1ce772d1ca8f90ea2
LucasZF <lucas-zimerman1@hotmail.com>
2021-10-17 16:27:37 -0300

    Update MT2D/_MSDOS/IO/MT2D_Dos_Keyboard.h

--- MT2D/_MSDOS/IO/MT2D_Dos_Keyboard.h
+++ MT2D/_MSDOS/IO/MT2D_Dos_Keyboard.h
@@ -1,9 +1,6 @@
 #if defined(__MSDOS__) || defined(__DOS__) && !defined(MTDOS_KEYH)
 #define MTDOS_KEYH
-<<<<<<< HEAD
 #include <MT2D/MT2D_Terminal_Define.h>
-=======
->>>>>>> test/dos-render
 int MT2D_Dos_Keyboard_keytouched();
 bool MT2D_Dos_Keyboard_touched();
 
